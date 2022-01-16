2dc907c9385d290a4142eb0a5ca99e3887735180
Thiago F <tcfialho@gmail.com>
2022-01-15 23:26:22 -0300

    chore: change sample to sqs

--- content/WebApi/WebApi.Application/Features/SendToQueue/SendToQueueCommand.cs
+++ content/WebApi/WebApi.Application/Features/SendToQueue/SendToQueueCommand.cs
@@ -10,9 +10,6 @@ namespace WebApi.Application.Features.SendToQueue
     public partial class SendToQueueCommand : IRequest<Result>
     {
         [Required]
-        public Guid Id { get; protected set; }
-
-        [Required]
         public string Description { get; set; }
     }
 }
---
+++ content/WebApi/WebApi.Application/Features/SendToQueue/SendToQueueMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using Herald.MessageQueue;
+
+namespace WebApi.Application.Features.SendToQueue
+{
+    public class SendToQueueMessage : MessageBase
+    {
+        public SendToQueueMessage()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; }
+        public string Description { get; set; }
+    }
+}
